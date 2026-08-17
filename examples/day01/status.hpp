ae63639c6e8cddf1be534f65741e6085e089af2e
FeaturelyShooter <757671750@qq.com>
2026-08-17 11:47:45 +0800

    feat(day01): publish build pipeline fundamentals

---
+++ examples/day01/status.hpp
@@ -0,0 +1,6 @@
+#ifndef CPP_LEARNING_DAY01_STATUS_HPP
+#define CPP_LEARNING_DAY01_STATUS_HPP
+
+int status_code();
+
+#endif
---
+++ solutions/day01/mission.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int mission_code() {
+    return 17;
+}
+
+int main() {
+    std::cout << "mission code: " << mission_code() << '\n';
+    return 0;
+}
